b7b5550655899f974bc5c39327f983acb733e67f
hryesf <houriye.esfahanian@gmail.com>
2024-05-30 13:50:22 +0330

    add Telescoping constructor pattern example

--- Item2/src/main/java/com/hry/Main.java
+++ Item2/src/main/java/com/hry/Main.java
@@ -2,6 +2,10 @@ package com.hry;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Telescoping Constructor
+        NutritionFactsTelescopingCons cocaCola1 =
+                new NutritionFactsTelescopingCons(240, 8, 100, 0, 35, 27);
+
     }
 }
